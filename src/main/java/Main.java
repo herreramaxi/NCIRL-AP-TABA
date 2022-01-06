0f8e05905de1b82a1227533fede99a5d13fda37d
Maxi <herreramaxi@gmail.com>
2022-01-06 21:49:19 +0000

    creating taba project fo AP

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")==null ?"49000":System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/api");
+        server.setHandler(context);
+        ServletHolder h = new ServletHolder(new ServletContainer());
+        h.setInitParameter("com.sun.jersey.config.property.packages", "resources");
+        context.addServlet(h, "/*");
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+}
+}
