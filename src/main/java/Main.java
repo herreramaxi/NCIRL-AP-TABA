6d3d5e48674d37002db7c6d2010f3e67ed734e74
Maxi <herreramaxi@gmail.com>
2022-01-06 23:44:41 +0000

    server simple html to get status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,28 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")==null ?"49000":System.getenv("PORT")));
+
+        Server server = new Server(Integer.valueOf(System.getenv("PORT") == null ? "49000" : System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setWelcomeFiles(new String[]{"index.html"});
+        resource_handler.setResourceBase(".");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
         context.setContextPath("/api");
-        server.setHandler(context);
+
+//        server.setHandler(context);
         ServletHolder h = new ServletHolder(new ServletContainer());
         h.setInitParameter("com.sun.jersey.config.property.packages", "resources");
         context.addServlet(h, "/*");
@@ -23,5 +37,5 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-}
+    }
 }
